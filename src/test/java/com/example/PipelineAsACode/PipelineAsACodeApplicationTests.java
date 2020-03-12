2d0aa656eab65ad7c80bf5e77aff6626edfa2921
Michal <michal.malczyk@gmail.com>
2020-03-12 18:09:20 +0100

    first commit

---
+++ src/test/java/com/example/PipelineAsACode/PipelineAsACodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PipelineAsACode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineAsACodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
